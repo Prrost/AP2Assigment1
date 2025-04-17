252322d582784320bc92961e497281ec09c7947d
Prrost <prrostik@gmail.com>
2025-04-17 13:22:45 +0500

    order

---
+++ order-service2/Routes/Routes.go
@@ -0,0 +1,219 @@
+package Routes
+
+import (
+	"context"
+	"fmt"
+	inventorypb "github.com/Prrost/assignment1proto/proto/inventory"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"log"
+	"order-service/Storage"
+	"order-service/domain"
+	"order-service/orderpb"
+)
+
+type OrderServiceServer struct {
+	orderpb.UnimplementedOrderServiceServer
+	InvClient inventorypb.InventoryServiceClient
+	Storage   Storage.Storage
+}
+
+func (s *OrderServiceServer) CreateOrder(ctx context.Context, req *orderpb.CreateOrderRequest) (*orderpb.CreateOrderResponse, error) {
+	if req.Amount < 1 {
+		return nil, fmt.Errorf("amount can't be less than 1")
+	}
+
+	order := domain.Order{
+		ProductID: int(req.ProductId),
+		UserID:    int(req.UserId),
+		Amount:    req.Amount,
+		Status:    "Created",
+	}
+
+	availableAmount, err := CheckProductAmount(int(req.ProductId), s.InvClient)
+	if err != nil {
+		return nil, status.Error(codes.Internal, "error getting product amount")
+	}
+
+	if availableAmount < order.Amount {
+		return nil, status.Error(codes.InvalidArgument, "not enough amount in inventory")
+	}
+
+	createdOrder, err := s.Storage.CreateOrderX(order)
+	if err != nil {
+		return nil, status.Error(codes.Internal, "error creating order")
+	}
+
+	err = ProductChange(order.Amount, order.ProductID, availableAmount, s.InvClient)
+	if err != nil {
+		return nil, err
+	}
+
+	return &orderpb.CreateOrderResponse{
+		Message: "Order created successfully",
+		Order: &orderpb.Order{
+			OrderId:   int32(createdOrder.ID),
+			UserId:    int32(createdOrder.UserID),
+			ProductId: int32(createdOrder.ProductID),
+			Amount:    createdOrder.Amount,
+			Status:    createdOrder.Status,
+		},
+	}, nil
+}
+
+func (s *OrderServiceServer) GetOrder(ctx context.Context, req *orderpb.GetOrderRequest) (*orderpb.GetOrderResponse, error) {
+	order, err := s.Storage.GetOrderByIDX(int(req.OrderId))
+	if err != nil {
+		return nil, status.Error(codes.NotFound, err.Error())
+	}
+
+	return &orderpb.GetOrderResponse{
+		Message: "Order retrieved successfully",
+		Order: &orderpb.Order{
+			OrderId:   int32(order.ID),
+			UserId:    int32(order.UserID),
+			ProductId: int32(order.ProductID),
+			Amount:    int64(order.Amount),
+			Status:    order.Status,
+		},
+	}, nil
+}
+
+func (s *OrderServiceServer) UpdateOrder(ctx context.Context, req *orderpb.UpdateOrderRequest) (*orderpb.UpdateOrderResponse, error) {
+	// Проверяем, существует ли заказ
+	order, err := s.Storage.GetOrderByIDX(int(req.OrderId))
+	if err != nil {
+		return nil, status.Error(codes.NotFound, "order not found")
+	}
+
+	tempAmount := order.Amount
+
+	// Проверяем, что количество товара >= 1
+	if req.Amount < 1 {
+		return nil, status.Error(codes.InvalidArgument, "amount can't be less than 1")
+	}
+
+	// Получаем текущий объем доступного продукта
+	availableAmount, err := CheckProductAmount(int(req.ProductId), s.InvClient)
+	if err != nil {
+		return nil, status.Error(codes.Internal, "error getting product amount")
+	}
+
+	// Проверяем, хватает ли доступного товара
+	if req.Amount > availableAmount {
+		return nil, status.Error(codes.InvalidArgument, "not enough product amount in inventory")
+	}
+
+	// Подготовка обновленных данных заказа
+	order.UserID = int(req.OrderId)
+	order.ProductID = int(req.ProductId)
+	order.Amount = req.Amount
+	order.Status = req.Status
+
+	// Обновление в базе данных
+	updatedOrder, err := s.Storage.UpdateOrderByIDX(int(req.OrderId), order)
+	if err != nil {
+		return nil, status.Error(codes.Internal, "error updating order")
+	}
+
+	// Изменяем количество товара в наличии
+	err = ProductChange(req.Amount-tempAmount, updatedOrder.ProductID, availableAmount, s.InvClient)
+	if err != nil {
+		return nil, status.Error(codes.Internal, "error updating product amount in inventory")
+	}
+
+	// Возвращаем обновленный заказ
+	return &orderpb.UpdateOrderResponse{
+		Message: "Order updated successfully",
+		Order: &orderpb.Order{
+			OrderId:   int32(updatedOrder.ID),
+			UserId:    int32(updatedOrder.UserID),
+			ProductId: int32(updatedOrder.ProductID),
+			Amount:    updatedOrder.Amount,
+			Status:    updatedOrder.Status,
+		},
+	}, nil
+}
+
+func (s *OrderServiceServer) ListAllOrders(ctx context.Context, req *orderpb.ListAllOrdersRequest) (*orderpb.ListAllOrdersResponse, error) {
+	orders, err := s.Storage.ListAllOrdersX()
+	if err != nil {
+		return nil, err
+	}
+
+	var grpcOrders []*orderpb.Order
+	for _, o := range orders {
+		grpcOrders = append(grpcOrders, &orderpb.Order{
+			OrderId:   int32(o.ID),
+			UserId:    int32(o.UserID),
+			ProductId: int32(o.ProductID),
+			Amount:    int64(o.Amount),
+			Status:    o.Status,
+		})
+	}
+
+	return &orderpb.ListAllOrdersResponse{
+		Orders: grpcOrders,
+	}, nil
+}
+
+func CheckProductAmount(productId int, InvClient inventorypb.InventoryServiceClient) (int64, error) {
+	const op = "CheckProductAmount"
+
+	ctx := context.Background()
+
+	res, err := InvClient.GetProductById(ctx, &inventorypb.GetByIdRequest{Id: int64(productId)})
+	if err != nil {
+		st, ok := status.FromError(err)
+		if ok {
+			log.Printf("[%s] grpc_status:%s/ error getting product by id: %s", op, st.Code(), err)
+			switch st.Code() {
+			case codes.NotFound:
+				log.Printf("[%s] product not found", op)
+			case codes.Internal:
+				log.Printf("[%s] internal error", op)
+			case codes.InvalidArgument:
+				log.Printf("[%s] invalid argument", op)
+			}
+		} else {
+			log.Printf("[%s] error getting product by id: %s", op, err)
+		}
+		return 0, nil
+	}
+
+	return res.Product.Amount, nil
+}
+
+func ProductChange(amount int64, productId int, inStorage int64, InvClient inventorypb.InventoryServiceClient) error {
+	const op = "ProductChange"
+
+	ctx := context.Background()
+	_, err := InvClient.UpdateProduct(ctx, &inventorypb.UpdateRequest{
+		Id:     int64(productId),
+		Name:   "",
+		Amount: inStorage - amount,
+	})
+	if err != nil {
+		st, ok := status.FromError(err)
+		if ok {
+			log.Printf("[%s] grpc_status:%s/ error updating product by id: %s", op, st.Code(), err)
+			switch st.Code() {
+			case codes.NotFound:
+				log.Printf("[%s] product not found", op)
+			case codes.Internal:
+				log.Printf("[%s] internal error", op)
+			case codes.InvalidArgument:
+				log.Printf("[%s] invalid argument", op)
+			case codes.AlreadyExists:
+				log.Printf("[%s] product already exists", op)
+			}
+		} else {
+			log.Printf("[%s] error updating product by id: %s", op, err)
+		}
+		return err
+	}
+
+	log.Printf("[%s] product updated", op)
+	return nil
+
+}
---
+++ order-service2/Storage/Sqlite.go
@@ -0,0 +1,116 @@
+package Storage
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+	"order-service/config"
+	"order-service/domain"
+)
+
+type SqliteStorage struct {
+	db *sql.DB
+}
+
+func NewSqliteStorage(cfg *config.Config) *SqliteStorage {
+
+	db, err := sql.Open("sqlite3", cfg.DBPath)
+	if err != nil {
+		log.Fatal(err)
+		return nil
+	}
+
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS orders (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    userid INTEGER NOT NULL,
+	productid INTEGER NOT NULL,
+    amount INTEGER NOT NULL,
+    status TEXT NOT NULL
+);`)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &SqliteStorage{
+		db: db,
+	}
+}
+
+func (s SqliteStorage) CreateOrderX(object domain.Order) (domain.Order, error) {
+
+	stmt, err := s.db.Prepare(`INSERT INTO orders (userid, productid, amount, status) VALUES (?, ?, ?, ?)`)
+	if err != nil {
+		return domain.Order{}, err
+	}
+	defer stmt.Close()
+
+	res, err := stmt.Exec(object.UserID, object.ProductID, object.Amount, object.Status)
+	if err != nil {
+		return domain.Order{}, err
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return domain.Order{}, err
+	}
+
+	object.ID = int(id)
+
+	return object, nil
+}
+
+func (s SqliteStorage) UpdateOrderByIDX(id int, object domain.Order) (domain.Order, error) {
+
+	stmt, err := s.db.Prepare(`UPDATE orders SET userid = ?, productid = ?, amount = ?, status = ? WHERE id = ?`)
+	if err != nil {
+		return domain.Order{}, err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(object.UserID, object.ProductID, object.Amount, object.Status, id)
+	if err != nil {
+		return domain.Order{}, err
+	}
+
+	return object, nil
+}
+
+func (s SqliteStorage) GetOrderByIDX(id int) (domain.Order, error) {
+	var object domain.Order
+
+	fmt.Println("id:", id)
+	err := s.db.QueryRow(`SELECT * FROM orders WHERE id = ?`, id).Scan(&object.ID, &object.UserID, &object.ProductID, &object.Amount, &object.Status)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return domain.Order{}, err
+		}
+		return domain.Order{}, err
+	}
+
+	return object, nil
+}
+
+func (s SqliteStorage) ListAllOrdersX() ([]domain.Order, error) {
+
+	rows, err := s.db.Query(`SELECT id, userid, productid, amount, status FROM orders`)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var orders []domain.Order
+
+	for rows.Next() {
+		var o domain.Order
+		err := rows.Scan(&o.ID, &o.UserID, &o.ProductID, &o.Amount, &o.Status)
+		if err != nil {
+			return nil, err
+		}
+		orders = append(orders, o)
+	}
+
+	return orders, nil
+
+}
